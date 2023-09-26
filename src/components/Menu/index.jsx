865478c8f29032b6d7b6855faca5739079a67dbf
shankar7042 <shankar.kumar.4171@gmail.com>
2023-09-26 17:04:50 +0530

    create ui for menu items

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,27 @@
+import { Eraser, FileDown, Pencil, Redo, Undo } from "lucide-react";
+import cn from "classnames";
+import styles from "./index.module.css";
+
+const Menu = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.iconContainer}>
+        <Pencil className={styles.icon} />
+      </div>
+      <div className={styles.iconContainer}>
+        <Eraser className={styles.icon} />
+      </div>
+      <div className={styles.iconContainer}>
+        <Undo className={styles.icon} />
+      </div>
+      <div className={styles.iconContainer}>
+        <Redo className={styles.icon} />
+      </div>
+      <div className={styles.iconContainer}>
+        <FileDown className={styles.icon} />
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
