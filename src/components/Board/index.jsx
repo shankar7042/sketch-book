74a564ad835ab5891e5638ca3ca2ed359b933302
shankar7042 <shankar.kumar.4171@gmail.com>
2023-09-28 02:51:20 +0530

    download feature bug fix

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -25,7 +25,7 @@ const Board = () => {
       a.click();
     }
     dispatch(actionItemClick(null));
-  }, [actionMenuItem]);
+  }, [actionMenuItem, dispatch]);
 
   useLayoutEffect(() => {
     if (!canvasRef.current) return;
