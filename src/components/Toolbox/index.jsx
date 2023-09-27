8205ce2140c4ed5a21c1df5ca9aac0cb4493ede4
shankar7042 <shankar.kumar.4171@gmail.com>
2023-09-27 17:24:12 +0530

    created toolbox slice and consumed it

--- src/components/Toolbox/index.jsx
+++ src/components/Toolbox/index.jsx
@@ -1,15 +1,28 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import cn from "classnames";
 
 import { COLORS, MENU_ITEMS } from "@/constants";
+import { changeBrushSize, changeColor } from "@/slice/toolboxSlice";
 
 import styles from "./index.module.css";
 
 const Toolbox = () => {
+  const dispatch = useDispatch();
+
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.PENCIL ||
     activeMenuItem === MENU_ITEMS.ERASER;
+
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+  };
+
+  const updateColor = (color) => {
+    dispatch(changeColor({ item: activeMenuItem, color }));
+  };
 
   return (
     <div className={styles.toolboxContainer}>
@@ -17,11 +30,14 @@ const Toolbox = () => {
         <div className={styles.toolItem}>
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
-            {Object.keys(COLORS).map((color) => (
+            {Object.keys(COLORS).map((colorKey) => (
               <div
-                key={color}
-                className={styles.colorBox}
-                style={{ backgroundColor: COLORS[color] }}
+                key={colorKey}
+                className={cn(styles.colorBox, {
+                  [styles.active]: color === COLORS[colorKey],
+                })}
+                style={{ backgroundColor: COLORS[colorKey] }}
+                onClick={() => updateColor(COLORS[colorKey])}
               />
             ))}
           </div>
@@ -39,6 +55,8 @@ const Toolbox = () => {
               max={10}
               step={1}
               className={styles.inputRange}
+              value={size}
+              onChange={updateBrushSize}
             />
           </div>
         </div>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from "@/slice/menuSlice";
+import toolboxReducer from "@/slice/toolboxSlice";
 
 export const store = configureStore({
   reducer: {
     menu: menuReducer,
+    toolbox: toolboxReducer,
   },
 });
